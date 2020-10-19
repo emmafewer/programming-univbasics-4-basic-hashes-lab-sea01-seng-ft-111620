2f9c380244369789e74b811fd0d829de4846e5ac
Emma Fewer <emmafewer20@gmail.com>
2020-10-19 17:24:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,18 @@
-def new_hash {
-}
+def new_hash 
+  {}
 end
 
-def my_hash {
-  :fruit => "apple"
-}
+def my_hash 
+  {:fruit => "apple"}
 end
 
-def pioneer {
-  :name => "Grace Hopper"
-}
+def pioneer 
+  {:name => "Grace Hopper"}
 end
 
 
-def id_hash_generator(number){
-  :id[number]
-}
+def id_hash_generator(number)
+  {:id[number]}
   
   # return a hash with a key :id assigned to the provided number
 end
